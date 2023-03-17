ebd3482411dad6f829c254a1c89420c52abaf017
Oderen <oleksandr.deren02@gmail.com>
2023-03-17 15:50:23 +0200

    2-task

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,31 +1,106 @@
-// Описаний в документації
+
+// Flatpickr
 import flatpickr from "flatpickr";
-// Додатковий імпорт стилів
 import "flatpickr/dist/flatpickr.min.css";
+
+// Notiflix
+import Notiflix from 'notiflix';
+
+const refs = {
+  input: document.querySelector('#datetime-picker'),
+  btn: document.querySelector('button[data-start]'),
+  days: document.querySelector('span[data-days]'),
+  hours: document.querySelector('span[data-hours]'),
+  minutes: document.querySelector('span[data-minutes]'),
+  seconds: document.querySelector('span[data-seconds]'),
+};
+
+refs.btn.setAttribute('disabled', true);
+
 
 const options = {
   enableTime: true,
   time_24hr: true,
+  enableSeconds: true,
   defaultDate: new Date(),
   minuteIncrement: 1,
-  onClose(selectedDates) {
-    console.log(selectedDates[0]);
+    onClose(selectedDates) {
+        const currentTime = new Date();
+        const isDateInFuture = selectedDates[0] - currentTime;
+
+      if (Math.sign(isDateInFuture) === -1) {
+        Notiflix.Notify.failure('Please choose a date in the future');
+        }
+
+      if (Math.sign(isDateInFuture) === 1) {
+        refs.btn.removeAttribute('disabled');
+
+        let isActive = false;
+            
+        refs.btn.addEventListener('click', () => {
+            
+          if (isActive) {
+                return;
+              }
+            
+            const intervalID = setInterval(() => {
+                
+              isActive = true;
+
+              const passedTime = new Date();
+              const deltaTime = selectedDates[0] - passedTime;
+              
+              const timer = convertMs(deltaTime);
+  
+              console.log(`${timer.days}:${timer.hours}:${timer.minutes}:${timer.seconds}`);   
+              updateTimer(timer);
+
+              if (deltaTime < 1000) {
+                clearInterval(intervalID);
+                resetTimer();
+              }
+            }, 1000);
+          })
+        }
   },
 };
 
-const refs = {
-    input: document.querySelector('#datetime-picker'),
-    btn: document.querySelector('button[data-start]'),
-}
+flatpickr('input#datetime-picker', options);
 
-refs.btn.setAttribute('disabled', true);
+function convertMs(ms) {
+  // Number of milliseconds per unit of time
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
 
-refs.input.addEventListener('change', (e) => {
-    console.log('Вікно закрилося');
-    
-    if (refs.btn.hasAttribute('disabled')) {
-        refs.btn.setAttribute('disabled', false);
-    }
-});
+  // Remaining days
+  const days = addLeadingZero(Math.floor(ms / day));
+  // Remaining hours
+  const hours = addLeadingZero(Math.floor((ms % day) / hour));
+  // Remaining minutes
+  const minutes = addLeadingZero(Math.floor(((ms % day) % hour) / minute));
+  // Remaining seconds
+  const seconds = addLeadingZero(Math.floor((((ms % day) % hour) % minute) / second));
 
-flatpickr("#datetime-picker");
+  return { days, hours, minutes, seconds };
+};
+
+const addLeadingZero = (value) => {
+  return String(value).padStart(2, '0');
+};
+
+function updateTimer(timer) {
+  refs.days.textContent = timer.days;
+  refs.hours.textContent = timer.hours;
+  refs.minutes.textContent = timer.minutes;
+  refs.seconds.textContent = timer.seconds;
+};
+
+function resetTimer() {
+  refs.days.textContent = '00';
+  refs.hours.textContent = '00';
+  refs.minutes.textContent = '00';
+  refs.seconds.textContent = '00';
+};
+
