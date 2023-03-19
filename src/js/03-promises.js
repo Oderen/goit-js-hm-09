2c5c79831d62618d72dfc8ad32369f496560ae57
Oderen <oleksandr.deren02@gmail.com>
2023-03-19 22:57:58 +0200

    task-3

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -8,6 +8,8 @@ const refs = {
 };
 
 let countNumber = 0;
+let totalMiliSeconds = 0;
+let firstIteration = true;
 
 refs.form.addEventListener('submit', (e) => {
   e.preventDefault();
@@ -15,66 +17,60 @@ refs.form.addEventListener('submit', (e) => {
   const firstDelay = Number(refs.firstDelay.value);
   const amount = Number(refs.amount.value);
   const regularDelay = Number(refs.regularDelay.value);
-  
+
   const intervalID = setInterval(() => {
     
     countNumber += 1;
     console.log('Номер ітерації: ', countNumber);
     const position = countNumber;
+
+    if (countNumber === 1) {
+      totalMiliSeconds = firstDelay;
+      console.log('totalMiliSeconds: ', totalMiliSeconds);
+    } else {
+      totalMiliSeconds += (firstDelay + regularDelay);
+      console.log('totalMiliSeconds: ', totalMiliSeconds);
+    }
     
-    createPromise(position, firstDelay, regularDelay)
+    createPromise(position, firstDelay, totalMiliSeconds)
       .then(OnSucess)
       .catch(OnFailure);
 
     if (countNumber === amount) {
       Notiflix.Notify.info('Цикл завершився');
       countNumber = 0;
+      totalMiliSeconds = 0;
       clearInterval(intervalID);
     }
   }, regularDelay);
+  
 }
 );
 
-function createPromise(position, firstDelay, delay) {
+function createPromise(position, firstDelay, totalMiliSeconds) {
   console.log('position: ', `${position}`);
-  console.log('delay: ', `${delay}`);
+  console.log('totalMiliSeconds: ', `${totalMiliSeconds}`);
   console.log('First Delay: ', `${firstDelay}`);
   
   return new Promise((resolve, reject) => { 
     const shouldResolve = Math.random() > 0.3;
-
+    
     setTimeout(() => { 
     if (shouldResolve) {
-    resolve(`✅ Fulfilled promise ${position} in ${delay}ms`);
+    resolve(`✅ Fulfilled promise ${position} in ${totalMiliSeconds}ms`);
   } else {
-    reject(`❌ Rejected promise ${position} in ${delay}ms`);
+    reject(`❌ Rejected promise ${position} in ${totalMiliSeconds}ms`);
       }
     }, firstDelay);
   });
 };
 
 function OnSucess(result) {
-   Notiflix.Notify.success(`${result}`);
+   return Notiflix.Notify.success(`${result}`);
 };
 
 function OnFailure(error) {
-  Notiflix.Notify.failure(`${error}`)
+  return Notiflix.Notify.failure(`${error}`);
 };
-
-
-//             Notify
-
-// Notiflix.Notify.success('Sol lucet omnibus');
-
-// Notiflix.Notify.failure('Qui timide rogat docet negare');
-
-// Notiflix.Notify.warning('Memento te hominem esse');
-
-// Notiflix.Notify.info('Cogito ergo sum');
-
-
-
-
-
 
 
