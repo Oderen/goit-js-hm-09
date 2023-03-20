c2280469549236692dc4765f64b337eac933a87b
Oderen <oleksandr.deren02@gmail.com>
2023-03-20 15:23:05 +0200

    task-3

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,5 +1,6 @@
 import Notiflix from 'notiflix';
 
+
 const refs = {
   form: document.querySelector('.form'),
   firstDelay: document.querySelector('input[name="delay"]'),
@@ -7,70 +8,36 @@ const refs = {
   amount: document.querySelector('input[name="amount"]'),
 };
 
-let countNumber = 0;
-let totalMiliSeconds = 0;
-let firstIteration = true;
+function onFormSubmit(evt) {
+  evt.preventDefault();
+  let delay = Number(refs.firstDelay.value);
+  for (let i = 1; i <= refs.amount.value; i += 1) {
+    createPromise(i, delay)
+    .then(({position, delay}) => {
+      Notiflix.Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+    })
+    .catch(({position, delay}) => {
+      Notiflix.Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
+    });
+    delay = delay + Number(refs.regularDelay.value);
+  }
+};
 
-refs.form.addEventListener('submit', (e) => {
-  e.preventDefault();
+refs.form.addEventListener('submit', onFormSubmit);
 
-  const firstDelay = Number(refs.firstDelay.value);
-  const amount = Number(refs.amount.value);
-  const regularDelay = Number(refs.regularDelay.value);
-
-  const intervalID = setInterval(() => {
-    
-    countNumber += 1;
-    console.log('Номер ітерації: ', countNumber);
-    const position = countNumber;
-
-    if (countNumber === 1) {
-      totalMiliSeconds = firstDelay;
-      console.log('totalMiliSeconds: ', totalMiliSeconds);
-    } else {
-      totalMiliSeconds += (firstDelay + regularDelay);
-      console.log('totalMiliSeconds: ', totalMiliSeconds);
-    }
-    
-    createPromise(position, firstDelay, totalMiliSeconds)
-      .then(OnSucess)
-      .catch(OnFailure);
-
-    if (countNumber === amount) {
-      Notiflix.Notify.info('Цикл завершився');
-      countNumber = 0;
-      totalMiliSeconds = 0;
-      clearInterval(intervalID);
-    }
-  }, regularDelay);
+function createPromise(position, delay) {
+  const shouldResolve = Math.random() > 0.3;
   
-}
-);
-
-function createPromise(position, firstDelay, totalMiliSeconds) {
-  console.log('position: ', `${position}`);
-  console.log('totalMiliSeconds: ', `${totalMiliSeconds}`);
-  console.log('First Delay: ', `${firstDelay}`);
-  
-  return new Promise((resolve, reject) => { 
-    const shouldResolve = Math.random() > 0.3;
-    
-    setTimeout(() => { 
-    if (shouldResolve) {
-    resolve(`✅ Fulfilled promise ${position} in ${totalMiliSeconds}ms`);
-  } else {
-    reject(`❌ Rejected promise ${position} in ${totalMiliSeconds}ms`);
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve({position, delay});
+      } else {
+        reject({position, delay});
       }
-    }, firstDelay);
-  });
+    }, delay);
+  })
 };
 
-function OnSucess(result) {
-   return Notiflix.Notify.success(`${result}`);
-};
-
-function OnFailure(error) {
-  return Notiflix.Notify.failure(`${error}`);
-};
 
 
